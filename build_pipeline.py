e4ff544ca7566620ca7f4d370e70abe336b8f89f
tpradeau <thomas.pradeau@ensae.fr>
2026-02-03 10:25:09 +0000

    Creation main et modules

--- build_pipeline.py
+++ build_pipeline.py
@@ -1,46 +1,8 @@
-"""
-Prediction de la survie d'un individu sur le Titanic
-"""
-
-import os
-from dotenv import load_dotenv
-import argparse
-
-import pandas as pd
-
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.metrics import confusion_matrix
-
-
-# ENVIRONMENT CONFIGURATION ---------------------------
-
-load_dotenv()
-
-parser = argparse.ArgumentParser(description="Paramètres du random forest")
-parser.add_argument(
-    "--n_trees", type=int, default=20, help="Nombre d'arbres"
-)
-args = parser.parse_args()
-
-n_trees = args.n_trees
-jeton_api = os.environ.get("JETON_API", "")
-data_path = os.environ.get("DATA_PATH", "data.csv")
-
-MAX_DEPTH = None
-MAX_FEATURES = "sqrt"
-
-if jeton_api.startswith("$"):
-    print("API token has been configured properly")
-else:
-    print("API token has not been configured")
-
-
-# FUNCTIONS --------------------------
 
 def create_pipeline(
     n_trees,
@@ -109,51 +71,3 @@ def create_pipeline(
     )
 
     return pipe
-
-
-def evaluate_model(pipe, X_test, y_test):
-    """
-    Evaluate the model by calculating the score and confusion matrix.
-
-    Args:
-        pipe (sklearn.pipeline.Pipeline): The trained pipeline object.
-        X_test (pandas.DataFrame): The test data.
-        y_test (pandas.Series): The true labels for the test data.
-
-    Returns:
-        tuple: A tuple containing the score and confusion matrix.
-    """
-    score = pipe.score(X_test, y_test)
-    matrix = confusion_matrix(y_test, pipe.predict(X_test))
-    return score, matrix
-
-
-
-# IMPORT ET STRUCTURATION DONNEES --------------------------------
-
-TrainingData = pd.read_csv("data.csv")
-
-y = TrainingData["Survived"]
-X = TrainingData.drop("Survived", axis="columns")
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.1
-)
-
-
-# PIPELINE ----------------------------
-
-pipe = create_pipeline(
-    n_trees, max_depth=MAX_DEPTH, max_features=MAX_FEATURES
-)
-
-
-# ESTIMATION ET EVALUATION ----------------------
-
-pipe.fit(X_train, y_train)
-score, matrix = evaluate_model(pipe, X_test, y_test)
-
-print(f"{score:.1%} de bonnes réponses sur les données de test pour validation")
-print(20 * "-")
-print("matrice de confusion")
-print(matrix)
